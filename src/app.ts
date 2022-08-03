b63ce828dcd0384ed52cb772bd60334b6614c52e
Emerson Patrik <epribeiro@clig.net.br>
2022-08-03 10:08:49 -0300

    Blaze Tips

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,6 @@ let socket: any
 function connect(){
     socket = makeConnectionBlaze({
         needCloseWithCompletedSession: false,
-        //token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTM4NTk3NTUsImJsb2NrcyI6W10sImlhdCI6MTY1ODM3Mjk1MCwiZXhwIjoxNjYzNTU2OTUwfQ.SS6oU6byFJStAZfOAi0jf6RzebzPzBrUpq2WHjkUkUw',
         type: 'doubles',
         requireNotRepeated: true,
     })
@@ -36,24 +35,6 @@ function connect(){
 }
 
 connect()
-
-// socket.ev.on('game_complete', async (msg) => {
-//     const { id, color, roll} = msg as DoubleUpdate
-
-//     const colorName = color === 0 ? "white" :  color === 1 ? "red" : "black"
-//     console.log({ id, colorName, roll })
-//     await axios.post("http://localhost:3000/colors", { colorName, number: roll })
-
-//     setTimeout(() => {
-//         socket.closeSocket()
-//     }, 5000)
-// })
-
-// socket.ev.on('close', (msg) => {
-//     console.log('Fechou a conexão - ' + JSON.stringify(msg))
-//     socket = connect()
-//     console.log(socket)
-// })
 
 const app = express()
 
