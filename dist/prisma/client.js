96dd9b27509f4952d312e432865b7cd99c49df91
Emerson Patrik <epribeiro@clig.net.br>
2022-08-03 10:24:04 -0300

    Blaze Double Tips

--- dist/prisma/client.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.client = void 0;
-var client_1 = require("@prisma/client");
-var client = new client_1.PrismaClient();
-exports.client = client;
--- dist/server.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-app_1.app.listen(process.env.PORT || 3000, function () { return console.log("✅ Server is running!"); });
